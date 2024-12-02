9c810984be328e5f5c6a9840c2d93f127188412e
kaw-web <kfatnassi.dev@gmail.com>
2024-12-02 14:49:11 +0100

    Rajout config pour refresh token

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
